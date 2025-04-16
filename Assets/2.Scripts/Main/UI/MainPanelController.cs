08ac147f4406ddc602c2cafee75dfb0e03da3824
Park Jiwoo <jiwoo4237@naver.com>
2025-04-16 16:17:55 +0900

    Main UI 틀 구현
    - 새 게임, 불러오기, 설정 버튼 생성 완료
    - SettingPanel (Slider, CloseButton) 구현 완료
    - 게임 Data 관련은 추후 구현 예정

---
+++ Assets/2.Scripts/Main/UI/MainPanelController.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using DG.Tweening;
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainPanelController : MonoBehaviour
+{
+    [SerializeField] private Transform Canvas;
+    [SerializeField] private GameObject SettingPanel;
+
+    private GameObject settingPanel;
+    
+    public void OnClickSettingButton()
+    {
+        if (settingPanel == null)
+            settingPanel = Instantiate(SettingPanel, Canvas);
+
+        settingPanel.SetActive(true);
+
+        CanvasGroup canvasGroup = settingPanel.GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 0;
+        canvasGroup.DOFade(1, 0.1f);
+    }
+
+    // 오프닝 연출
+    public void OnSceneLoaded()
+    {
+        GameObject background = GameObject.Find("Background");
+        GameObject cavin = GameObject.Find("Cavin");
+        GameObject title = GameObject.Find("Title");
+        GameObject subTitle = GameObject.Find("SubTitle");
+        GameObject buttons = GameObject.Find("Buttons");
+
+        background.SetActive(true);
+        cavin.SetActive(false);
+        title.SetActive(false);
+        subTitle.SetActive(false);
+        buttons.SetActive(false);
+
+        // 클릭 시 연출 skip
+        if (Input.GetMouseButtonDown(0))
+        {
+            cavin.SetActive(true);
+            title.SetActive(true);
+            subTitle.SetActive(true);
+            buttons.SetActive(true);
+            return;
+        }
+
+        // BGM (FadeIn)
+
+        // Cavin Image 등장 (FadeIn)
+        cavin.SetActive(true);
+        cavin.GetComponent<SpriteRenderer>().DOFade(0, 0);
+        cavin.GetComponent<SpriteRenderer>().DOFade(1, 0.5f);
+
+        // Title 등장 (FadeIn)
+        title.SetActive(true);
+        title.GetComponent<SpriteRenderer>().DOFade(0, 0);
+        title.GetComponent<SpriteRenderer>().DOFade(1, 0.5f).OnComplete(() =>
+        {
+            // Title 글자 하나씩 등장 (FadeIn)
+            for (int i = 0; i < title.transform.childCount; i++)
+            {
+                Transform letter = title.transform.GetChild(i);
+                letter.gameObject.SetActive(true);
+                letter.GetComponent<SpriteRenderer>().DOFade(0, 0);
+                letter.GetComponent<SpriteRenderer>().DOFade(1, 0.5f).SetDelay(i * 0.2f);
+            }
+        });
+
+        // SubTitle 등장 (FadeIn)
+        subTitle.SetActive(true);
+        subTitle.GetComponent<SpriteRenderer>().DOFade(0, 0);
+        subTitle.GetComponent<SpriteRenderer>().DOFade(1, 0.5f).OnComplete(() =>
+        {
+            // SubTitle 글자 하나씩 등장 (FadeIn)
+            for (int i = 0; i < subTitle.transform.childCount; i++)
+            {
+                Transform letter = subTitle.transform.GetChild(i);
+                letter.gameObject.SetActive(true);
+                letter.GetComponent<SpriteRenderer>().DOFade(0, 0);
+                letter.GetComponent<SpriteRenderer>().DOFade(1, 0.5f).SetDelay(i * 0.2f);
+            }
+        });
+
+        // Buttons 등장 (FadeIn)
+        buttons.SetActive(true);
+        buttons.GetComponent<CanvasGroup>().alpha = 0;
+        buttons.GetComponent<CanvasGroup>().DOFade(1, 0.5f).OnComplete(() =>
+        {
+            // Buttons 클릭 가능
+            buttons.GetComponentInChildren<Button>().interactable = true;
+        });
+    }
+}

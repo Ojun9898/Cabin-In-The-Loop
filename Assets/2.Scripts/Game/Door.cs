3c78e9e39a0035d9b7e9997b2048034270f5b757
Park Jiwoo <jiwoo4237@naver.com>
2025-05-12 12:57:14 +0900

    [추가] 맵 내의 엘레베이터, Door 기능
    [추가] 맵 내의 엘레베이터, Door 기능

---
+++ Assets/2.Scripts/Game/Door.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private Animation anim;
+    [SerializeField] private AudioSource audioSource;
+
+    private bool isDoorOpen = false;
+    private bool isPlayerInRange = false;
+    
+    private void Update()
+    {
+        if (isPlayerInRange && Input.GetKeyDown(KeyCode.E) && !isDoorOpen)
+        {
+            anim.Play();
+            audioSource.Play(); // 문 열리는 사운드 재생
+            isDoorOpen = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            isPlayerInRange = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            isPlayerInRange = false;
+    }
+}

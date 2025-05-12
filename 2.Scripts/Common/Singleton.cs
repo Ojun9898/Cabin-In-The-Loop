94e61a476596c8916687e60e96554f79655075ac
hml4740 <hml4740@naver.com>
2025-05-12 14:17:05 +0900

    DamageFieldEffect.cs 추가
    데미지 필드 이펙트 스크립트 추가

---
+++ 2.Scripts/Common/Singleton.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = FindObjectOfType<T>();
+
+            if (_instance == null)
+                Debug.LogError($"[Singleton] Instance of {typeof(T)} not found in the scene.");
+
+            return _instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ 2.Scripts/Game/Player/Weapon/WeaponType.cs
@@ -0,0 +1,20 @@
+﻿public enum WeaponType
+{
+    Axe,
+    CrowBar,
+    HandGun,
+    
+    // MeleeWeapon
+    BaseballBat,
+    CrowBar2,
+
+    // Ranged weapon
+    AK47M,
+    M1911,
+
+    // Auxiliary weapon
+    Grenade,
+    Knife,
+    
+    // 필요 시 계속 추가
+}
